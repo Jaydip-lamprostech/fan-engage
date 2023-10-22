defecc4a03662948ea7bba3714b782d70b524d15
Jaydip Patel <jaydip.lamprostech@gmail.com>
2023-10-22 12:53:28 +0530

    scan component modified

--- src/components/ExtraParentComponent.js
+++ src/components/ExtraParentComponent.js
@@ -9,7 +9,15 @@ function ExtraParentComponent(props) {
         className="close-extra-main"
         onClick={() => props.setExtraComponent("")}
       >
-        close
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          style={{ fill: "white" }}
+        >
+          <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
+        </svg>
       </div>
       {props.extraComponent === "scan-ticket" ? <ScanTicket /> : null}
     </div>
--- src/components/ScanTicket.js
+++ src/components/ScanTicket.js
@@ -1,9 +1,16 @@
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import React, { useState } from "react";
 import "../styles/ScanTicket.css";
+import dummyReward from "../assets/dummy_reward02.jpg";
 
 function ScanTicket() {
   const [msg, setmsg] = useState("");
+  // {
+  //   eventName: "Music Festival",
+  //   event_venue: "address",
+  //   ticketImage: "2023-10-28",
+  //   collection_name: "artist collection",
+  // }
   return (
     <div className="scan-ticket-main-component">
       <QrScanner
@@ -14,7 +21,45 @@ function ScanTicket() {
           setmsg(error?.message);
         }}
       />
-      <div>{msg ? <p>{msg}</p> : null}</div>
+      <div>
+        {msg ? (
+          <div className="mint-nft-main">
+            <h2>Ticket Details</h2>
+            <div className="ticket-details-item">
+              <span className="item-title">Event Name</span>
+              <span className="item-value">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,
+                velit?
+              </span>
+            </div>
+            <div className="ticket-details-item">
+              <span className="item-title">Event Venue</span>
+              <span className="item-value">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,
+                velit?
+              </span>
+            </div>
+            <div className="ticket-details-item">
+              <span className="item-title">Collection Name</span>
+              <span className="item-value">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo,
+                velit?
+              </span>
+            </div>
+            <div className="ticket-details-item">
+              <span className="item-title">Event Ticket</span>
+              <span className="item-value">
+                <img src={dummyReward} alt="" />
+              </span>
+            </div>
+            <div className="ticket-details-item">
+              <span className="item-value mint-nft">
+                <button>Mint NFT</button>
+              </span>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
