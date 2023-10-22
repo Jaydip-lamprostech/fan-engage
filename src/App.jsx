18a1675cdab463e888d1c10a28034e3da0f4db16
Saurabh <saurabhjha1402@gmail.com>
2023-10-22 11:14:39 +0530

    pushing the databse code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { Database } from "@tableland/sdk";
 import { Wallet, getDefaultProvider } from "ethers";
+import "./App.css";
 
 function App() {
-  const [tableName, setTableName] = useState("");
-  const privateKey =
-    "c61829f03ee914ab79f60b4ce481006b6142af4c862324dee4bbaafb07bdc992";
+  const privateKey = import.meta.env.VITE_PRIVATE_KEY;
   const wallet = new Wallet(privateKey);
   const provider = getDefaultProvider(
     "https://polygon-mumbai.g.alchemy.com/v2/NeTaVDMNuGBLmW387WgXEvHrnLxRURwE"
@@ -15,74 +14,97 @@ function App() {
   // Connect to the database
   const db = new Database({ signer });
 
-  const prefix = "artist_data"; // Your custom table prefix
+  const FanPrefix = "Fantable_80001_8169";
+  const ArtistPrefix = "Artisttable_80001_8168";
 
-  const createTable = async () => {
+  const createArtistTable = async () => {
     console.log("Creating table...");
     const { meta: create } = await db
       .prepare(
-        `CREATE TABLE ${prefix} (id integer primary key, eoa text, event_name text, event_venue text, pass_image text, pass_count integer , collection_name text );`
+        `CREATE TABLE ${ArtistPrefix} (id integer primary key, eoa text, event_name text, event_venue text, pass_image text, pass_count text, collection_name text);`
       )
       .run();
 
-    // The table's `name` is in the format `{prefix}_{chainId}_{tableId}`
     console.log("Table created:", create.txn.name);
-    // tableName = create.txn.name;
-    setTableName(create.txn.name);
-    console.log(tableName);
   };
 
-  const instertTable = async () => {
-    console.log("inserting...");
-
-    if (!tableName) {
-      setTableName("artist_data_80001_7819");
-    }
-    console.log(tableName);
-
-    // Insert a row into the table
-
-    const { meta: insert } = await db
+  const createFanTable = async () => {
+    console.log("Creating table...");
+    const { meta: create } = await db
       .prepare(
-        `INSERT INTO ${tableName} (eoa, event_name, event_venue, pass_image, pass_count, collection_name ) VALUES (?, ?, ? ,?, ?, ?);`
-      )
-
-      .bind(
-        "6679e259bbe30e2e50adb35882d87cae42612237d64f4bdd3bc52b1078cae77",
-        "Lollapalooza",
-        "Chicago",
-        "pass_image_1",
-        "28000",
-        "Collection_1"
+        `CREATE TABLE ${FanPrefix} (id integer primary key, eoa text, event_name text, event_venue text, scanned_image text, rewards text, collection_name text);`
       )
       .run();
 
-    // Wait for the insert operation to complete
-    // await insert;
+    console.log("Table created:", create.txn.name);
+  };
 
-    console.log("inserted...");
+  const insertFanTable = async () => {
+    console.log("Inserting...");
 
-    // Perform a read query, requesting all rows from the table
-    console.log("reading");
-    const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
+    // Insert a row into the table
+    const { meta: insert } = await db
+      .prepare(
+        `INSERT INTO ${FanPrefix} (eoa, event_name, event_venue, scanned_image, rewards, collection_name) VALUES (?, ?, ?, ?, ?, ?);`
+      )
+      .bind(
+        "0xB5204aff106dc1Ffc6bE909c94a6A933081dB636",
+        "La_tomatina with indian test",
+        "FUN BLAST, SBR road , Ahmedabad 380054,",
+        "https://images.squarespace-cdn.com/content/v1/58eb4273c534a5cb83c85625/1529762397780-L4B62P4PX6BA45AE8OKX/Tomatina+4.jpg?format=1500w",
+        "$10000",
+        "collection-1"
+      )
+      .run();
+
+    console.log("Inserted...");
+  };
+
+  const insertArtistTable = async () => {
+    console.log("Inserting...");
+
+    // Insert a row into the table
+    const { meta: insert } = await db
+      .prepare(
+        `INSERT INTO ${ArtistPrefix} (eoa, event_name, event_venue, pass_image, pass_count, collection_name) VALUES (?, ?, ?, ?, ?, ?);`
+      )
+      .bind(
+        "0xB5204aff106dc1Ffc6bE909c94a6A933081dB636",
+        "La_tomatina with indian test from Artist",
+        "Sarojini nagar, Delhi,",
+        "https://images.squarespace-cdn.com/content/v1/58eb4273c534a5cb83c85625/1529762397780-L4B62P4PX6BA45AE8OKX/Tomatina+4.jpg?format=1500w",
+        "10000",
+        "collection-1"
+      )
+      .run();
+
+    console.log("Inserted...");
+  };
+  const readArtistTable = async () => {
+    console.log("Reading Artist Data...");
+    const { results } = await db
+      .prepare(`SELECT * FROM ${ArtistPrefix};`)
+      .all();
     console.table(results);
   };
 
-  const ReadTable = async () => {
-    console.log("reading...");
-    if (!tableName) {
-      setTableName("my_demo_tables_80001_7819");
-    }
-    const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
+  const readFanTable = async () => {
+    console.log("Reading Fan Data...");
+    const { results } = await db.prepare(`SELECT * FROM ${FanPrefix}`).all();
     console.table(results);
   };
 
   return (
     <div>
       <h1>Hello From tableland</h1>
-      <button onClick={() => createTable()}>Create Table</button>
-      <button onClick={() => ReadTable()}>Read Table</button>
-      <button onClick={() => instertTable()}>insert Table</button>
+      <button onClick={() => createArtistTable()}>Create Artist Table</button>
+      <button onClick={() => createFanTable()}>Create Fan Table </button>
+      <button onClick={() => insertArtistTable()}>Insert Artist Table </button>
+
+      <button onClick={() => insertFanTable()}>Insert fan Table </button>
+      <button onClick={() => readFanTable()}>read fan Table </button>
+
+      <button onClick={() => readArtistTable()}>read Artist Table </button>
     </div>
   );
 }
