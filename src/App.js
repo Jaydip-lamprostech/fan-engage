30d79af0e43a4d7f967ba5f48f8c099d41d28195
Jaydip Patel <jaydip.lamprostech@gmail.com>
2023-10-25 11:54:53 +0530

    update the collections page

--- src/App.js
+++ src/App.js
@@ -26,16 +26,39 @@ function App() {
   const [activeComponent, setActiveComponent] = useState("My Rewards");
   const [extraComponent, setExtraComponent] = useState("");
   const [address, setAddress] = useState();
+  const [isCompatible, setIsCompatible] = useState(true);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsCompatible(false);
+      } else {
+        setIsCompatible(true);
+      }
+    };
+
+    // Add event listener to listen for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Call handleResize once on component mount
+    handleResize();
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="App">
-      <div className="home-page">
-        <MobileHeader
-          component={activeComponent}
-          setAddress={setAddress}
-          address={address}
-        />
-        {/* <div style={{ marginTop: "100px" }}>
+      {isCompatible ? (
+        <>
+          <div className="home-page">
+            <MobileHeader
+              component={activeComponent}
+              setAddress={setAddress}
+              address={address}
+            />
+            {/* <div style={{ marginTop: "100px" }}>
         <button onClick={authKitSignData}>Login</button>
       </div>
       <div>
@@ -44,34 +67,43 @@ function App() {
       <div>
         <button onClick={deploySafe}>Deploy Safe</button>
       </div> */}
-        {activeComponent === "My Rewards" ? (
-          <MyRewards address={address} />
-        ) : activeComponent === "Collections" ? (
-          <Collections address={address} />
-        ) : activeComponent === "Rank" ? (
-          <Rank address={address} />
-        ) : activeComponent === "Profile" ? (
-          <Profile address={address} />
-        ) : null}
-        <ScanTicketBottomComponent
-          showScanTicketComponent={showScanTicketComponent}
-          setShowScanTicketComponent={setShowScanTicketComponent}
-          setExtraComponent={setExtraComponent}
-          address={address}
-        />
-        <MobileFooter
-          setActiveComponent={setActiveComponent}
-          activeComponent={activeComponent}
-          setShowScanTicketComponent={setShowScanTicketComponent}
-          address={address}
-        />
-      </div>
-      {extraComponent && (
-        <ExtraParentComponent
-          setExtraComponent={setExtraComponent}
-          extraComponent={extraComponent}
-          address={address}
-        />
+            {activeComponent === "My Rewards" ? (
+              <MyRewards address={address} />
+            ) : activeComponent === "Collections" ? (
+              <Collections address={address} />
+            ) : activeComponent === "Rank" ? (
+              <Rank address={address} />
+            ) : activeComponent === "Profile" ? (
+              <Profile address={address} />
+            ) : null}
+            <ScanTicketBottomComponent
+              showScanTicketComponent={showScanTicketComponent}
+              setShowScanTicketComponent={setShowScanTicketComponent}
+              setExtraComponent={setExtraComponent}
+              address={address}
+            />
+            <MobileFooter
+              setActiveComponent={setActiveComponent}
+              activeComponent={activeComponent}
+              setShowScanTicketComponent={setShowScanTicketComponent}
+              address={address}
+            />
+          </div>
+          {extraComponent && (
+            <ExtraParentComponent
+              setExtraComponent={setExtraComponent}
+              extraComponent={extraComponent}
+              address={address}
+            />
+          )}
+        </>
+      ) : (
+        <div className="app-not-compatible">
+          <p>
+            This app is not compatible with large screens. Please use it on a
+            mobile or small screen.
+          </p>
+        </div>
       )}
     </div>
   );
--- src/components/Collections.js
+++ src/components/Collections.js
@@ -27,17 +27,16 @@ function Collections({ address }) {
       let collection2 = [];
       for (let i = 0; i < results.length; i++) {
         console.log(results[i]);
-        if (results[i].eoa === address.eoa) {
-          if (!data2.find((item) => results[i].id === item.id)) {
-            data2.push(results[i]);
-          }
-          if (
-            !collection2.find(
-              (item) => results[i].collection_name === item.collection_name
-            )
-          ) {
-            collection2.push(results[i].collection_name);
-          }
+
+        if (!data2.find((item) => results[i].id === item.id)) {
+          data2.push(results[i]);
+        }
+        if (
+          !collection2.find(
+            (item) => results[i].collection_name === item.collection_name
+          )
+        ) {
+          collection2.push(results[i].collection_name);
         }
       }
       console.log(data);
@@ -90,19 +89,26 @@ function Collections({ address }) {
             <select value={selectedOption} onChange={handleSelectChange}>
               <option value="">Select a Collection</option>
               {collections.map((item, key) => {
-                return <option value={item}>{item}</option>;
+                return (
+                  <option value={item} key={key}>
+                    {item}
+                  </option>
+                );
               })}
             </select>
           </div>
         ) : (
-          "Loading ....."
+          <div className="collections-list">
+            <select value={selectedOption}>
+              <option value="">Select a Collection</option>
+            </select>
+          </div>
         )}
       </div>
       <div className="collections-container">
-        <h2>Collection: "{selectedOption}"</h2>
+        {selectedOption ? <h2>Collection: "{selectedOption}"</h2> : null}
         {/* collection rewards container  */}
-        {/* {collectionData.length > 0 ? ( */}
-        {
+        {collectionData.length > 0 ? (
           collectionData.map((item, key) => {
             return (
               <div className="collections-grid-main">
@@ -118,30 +124,29 @@ function Collections({ address }) {
               </div>
             );
           })
-          // ) : (
-          //   <div className="no-rewards-container">
-          //     <div className="no-rewards-icon">
-          //       <svg
-          //         xmlns="http://www.w3.org/2000/svg"
-          //         height="24"
-          //         viewBox="0 -960 960 960"
-          //         width="24"
-          //         className="icon"
-          //       >
-          //         <path d="m354-247 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-80l65-281L80-550l288-25 112-265 112 265 288 25-218 189 65 281-247-149L233-80Zm247-350Z" />
-          //       </svg>
-          //     </div>
-          //     <div className="no-rewards-info">
-          //       <span className="no-rewards-info-title">
-          //         No NFTs found for this Collection. Try a different collection.
-          //       </span>
-          //       <span className="no-rewards-info-description">
-          //         Start by scanning your ticket and mint the nft.
-          //       </span>
-          //     </div>
-          //   </div>
-          // )
-        }
+        ) : (
+          <div className="no-rewards-container">
+            <div className="no-rewards-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 -960 960 960"
+                width="24"
+                className="icon"
+              >
+                <path d="m354-247 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-80l65-281L80-550l288-25 112-265 112 265 288 25-218 189 65 281-247-149L233-80Zm247-350Z" />
+              </svg>
+            </div>
+            <div className="no-rewards-info">
+              <span className="no-rewards-info-title">
+                No NFTs found for this Collection. Try a different collection.
+              </span>
+              <span className="no-rewards-info-description">
+                Start by scanning your ticket and mint the nft.
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
